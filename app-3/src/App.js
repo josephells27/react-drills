9f03f97d75b898bc2b58c09a033af41a652bf5a1
Joseph Ells <josephells27@gmail.com>
2018-06-10 15:16:49 -0600

    react-drills app-3 done

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -3,19 +3,42 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      filteredString: "",
+      foods:[
+        "candy",
+        "black tar heroine",
+        "korean dog meat",
+        "vitamin b12",
+        "spicy pickles",
+        "a single banana"
+      ]
+    }
+  }
+
+  handleChange(value) {
+    this.setState({filteredString:value})
+  }
+
   render() {
+    let newFoods = this.state.foods.filter((element, index) => {
+      return element.includes(this.state.filteredString)
+    }).map((element, index) => {
+      return <h2 key = {index}>{element}</h2>
+    })
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange = {(e) => this.handleChange(e.target.value)}/>
+        {newFoods}
       </div>
     );
   }
 }
+    
+
+    
 
 export default App;
