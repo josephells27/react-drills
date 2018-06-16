d39107cc1140a1fe3fa5b439d7cbdb440e778098
Joseph Ells <josephells27@gmail.com>
2018-06-16 00:17:32 -0600

    react-drills app-6 done

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Todo from './Todo'
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      list:[],
+      input:""
+    }
+  }
+
+  handleOnClick = () => {
+    this.setState({
+      list:[ this.state.list, this.state.input],
+      input:""
+    })
+  }
+
+  handleInputChange(value){
+    this.setState({input:value})
+  }
+
   render() {
+
+    let  list = this.state.list.map((el, i) => {
+      return( <Todo key={i} task={el} />
+      )
+    })
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <h3>this is my task list</h3>
+
+        <input value={this.state.input}
+        onChange={ e=> this.handleInputChange(e.target.value)}/>
+        <button onClick={this.handleOnClick}>Add</button>
+
+        { list }
+
       </div>
     );
   }
